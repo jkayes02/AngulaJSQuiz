8f94a1ab48a2da45a40a19aa4ea1c61f839b3007
James <jkayes02@gmail.com>
2017-08-02 13:24:44 +0100

    Quiz progress
    continue button increments question.

--- JS/controllers/quiz.js
+++ JS/controllers/quiz.js
@@ -3,10 +3,36 @@
 
     app.controller("quizCtrl", quizController);
 
+
     function quizController(quizMetrics, dataService) {
+        var questionsAnswered = 0;
+        var quizLength = dataService.questions.length;
+
         this.quizMetrics = quizMetrics;
         this.dataService = dataService;
         this.activeQuestion = 0;
+        this.setActiveQuestion = function() {
+            while(true) {
+                this.activeQuestion++;
+                if(this.activeQuestion >= quizLength) {
+                    this.activeQuestion = 0;
+                    break;
+                }
+                if(dataService.questions[this.activeQuestion].selected === null) { // If question has NOT been answered:
+                    break;
+                }
+            }
+            
+        };
+        this.questionAnswered = function() {
+            if(dataService.questions[this.activeQuestion].selected !== null) {
+                questionsAnswered++;
+                if(questionsAnswered >= quizLength) {
+
+                }
+            }
+            this.setActiveQuestion();
+        };
     };
     quizController.$inject = ['quizMetrics', 'dataService'];
 })();
